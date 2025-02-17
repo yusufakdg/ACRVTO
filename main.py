14e39116ae52f189467d4868519ac2f51d668d0f
MUHAMMED YUSUF AKDAG <muhammedyusufakdag@192.168.1.109>
2025-02-17 13:48:43 +0300

    Force add all files and update Dockerfile

--- main.py
+++ main.py
@@ -21,3 +21,13 @@ async def upload_file(file: UploadFile = File(...)):
         return {"message": f"File '{file.filename}' uploaded successfully."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+import os, json
+cred = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+print("GOOGLE_APPLICATION_CREDENTIALS:", repr(cred))
+service_account_info = json.loads(cred)
+
+import json
+with open("stately-century-300912-dc5bc153c25c.json", "r") as f:
+    service_account_info = json.load(f)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = json.dumps(service_account_info)
